7bdb50db9fe647e789242305a84cc4c9fc6c2750
neo-afalku <saidanis.khouider@outlook.fr>
2025-09-26 12:30:07 +0100

    updating the projeect

---
+++ client/src/components/ProductList.jsx
@@ -0,0 +1,10 @@
+import ProductItem from "./ProductItem";
+const ProductList = () => {
+  return (
+    <div className="product-list">
+      <ProductItem />
+    </div>
+  );
+};
+
+export default ProductList;
